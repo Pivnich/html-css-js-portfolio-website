f236e4eed1c2279b3826483ce208db86d3ffa5ac
ponochovny <ponochovny@gmail.com>
2026-03-22 01:36:23 +0200

    refactor: react + framer motion -aos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
 // const isProd = process.env.CI === 'true'
 
-const config = {}
-
-// if (isProd) {
-config.site = 'https://vulchyn.com'
-config.base = ''
-// }
+const config = {
+	integrations: [react()],
+	site: 'https://vulchyn.com',
+	base: '',
+}
 
 // https://astro.build/config
 export default defineConfig(config)
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,10 +1,5 @@
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
 // Import modular functionality
 import './modules/accordion'
 import './modules/back-to-top'
 import './modules/modal'
 import './modules/menu'
-
-AOS.init()
